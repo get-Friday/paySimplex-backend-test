aeb5ccfe077df47de9af75a56fa379a522ca34e2
get.Friday <gabriel.getdev@gmail.com>
2023-04-06 19:09:18 -0300

    Feat: create model chore

---
+++ paySimplex.Domain/Models/Chore.cs
@@ -0,0 +1,17 @@
+﻿using paySimplex.Domain.Enums;
+
+namespace paySimplex.Domain.Models
+{
+    internal class Chore
+    {
+        public int Id { get; internal set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Status Status { get; set; }
+        public Chore()
+        {
+        }
+    }
+}
