530076fd5498ecc92392f98b91ef6c8333e542d3
get.Friday <gabriel.getdev@gmail.com>
2023-04-06 19:09:09 -0300

    Feat: create model user

---
+++ paySimplex.Domain/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace paySimplex.Domain.Models
+{
+    internal class User
+    {
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
