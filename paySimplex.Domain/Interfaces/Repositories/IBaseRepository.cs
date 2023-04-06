60f8ad09b6cc0d41fed3f091f13b61f60d30f536
get.Friday <gabriel.getdev@gmail.com>
2023-04-06 19:31:48 -0300

    Feat: interface base repository

---
+++ paySimplex.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace paySimplex.Domain.Interfaces.Repositories
+{
+    internal interface IBaseRepository<TEntity, Tkey> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(Tkey id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
