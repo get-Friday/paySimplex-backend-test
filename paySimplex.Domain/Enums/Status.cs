929da70faf4e96f8562340c93efc0f72410b5df6
get.Friday <gabriel.getdev@gmail.com>
2023-04-06 19:09:33 -0300

    Feat: create enum status

---
+++ paySimplex.Domain/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace paySimplex.Domain.Enums
+{
+    internal enum Status
+    {
+        [Display(Name = "Scheduled")]
+        Scheduled,
+        [Display(Name = "In Progress")]
+        InProgress,
+        [Display(Name = "Done")]
+        Done
+    }
+}
