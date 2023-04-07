12b19a6a36f6e74860717cdda2d724d10b15b993
get.Friday <gabriel.getdev@gmail.com>
2023-04-07 16:59:32 -0300

    Feat: params on GetAll and GetByUserId on chore service interface

--- paySimplex.Domain/Interfaces/Services/IChoreService.cs
+++ paySimplex.Domain/Interfaces/Services/IChoreService.cs
@@ -1,10 +1,12 @@
 ﻿using paySimplex.Domain.DTOs;
+using paySimplex.Domain.Enums;
 
 namespace paySimplex.Domain.Interfaces.Services
 {
     public interface IChoreService
     {
-        IList<ChoreDTO> GetAll();
+        IList<ChoreDTO> GetAll(int? userId, Status? status, DateTime? startDate, DateTime? endDate);
+        IList<ChoreDTO> GetByUserId(int userId);
         ChoreDTO GetById(int id);
         void Insert(ChoreDTO chore);
         void Update(ChoreDTO chore, int id);
