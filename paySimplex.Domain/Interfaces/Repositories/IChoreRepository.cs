6491c548ffce4f387790bba59b2c98d203da6d0d
get.Friday <gabriel.getdev@gmail.com>
2023-04-06 22:52:12 -0300

    Feat: interface and repository for chore

---
+++ paySimplex.Domain/Interfaces/Repositories/IChoreRepository.cs
@@ -0,0 +1,8 @@
+﻿using paySimplex.Domain.Models;
+
+namespace paySimplex.Domain.Interfaces.Repositories
+{
+    internal interface IChoreRepository : IBaseRepository<Chore, int>
+    {
+    }
+}
---
+++ paySimplex.Infra/Data/Repositories/ChoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using paySimplex.Domain.Interfaces.Repositories;
+using paySimplex.Domain.Models;
+
+namespace paySimplex.Infra.Data.Repositories
+{
+    public class ChoreRepository : BaseRepository<Chore, int>, IChoreRepository
+    {
+        public ChoreRepository(PaySimplexDbContext context) : base(context)
+        {
+        }
+    }
+}
