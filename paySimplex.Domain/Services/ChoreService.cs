3eda3fc0cc703ce55448fa262ace6e006d92a257
get.Friday <gabriel.getdev@gmail.com>
2023-04-07 16:58:55 -0300

    Feat: implement methods on chore service

--- paySimplex.Domain/Services/ChoreService.cs
+++ paySimplex.Domain/Services/ChoreService.cs
@@ -1,6 +1,8 @@
 ﻿using paySimplex.Domain.DTOs;
+using paySimplex.Domain.Enums;
 using paySimplex.Domain.Interfaces.Repositories;
 using paySimplex.Domain.Interfaces.Services;
+using paySimplex.Domain.Models;
 
 namespace paySimplex.Domain.Services
 {
@@ -12,29 +14,66 @@ namespace paySimplex.Domain.Services
             _choreRepository = choreRepository;
         }
 
-        public IList<ChoreDTO> GetAll()
+        public IList<ChoreDTO> GetAll(int? userId, Status? status, DateTime? startDate, DateTime? endDate)
         {
-            throw new NotImplementedException();
+            IQueryable<Chore> query = _choreRepository.GetAll();
+
+            if (userId.HasValue)
+                query = query.Where(c => c.UserId == userId);
+
+            if (status.HasValue)
+                query = query.Where(c => c.Status == status);
+
+            if (startDate.HasValue)
+                query = query.Where(c => c.StartDate == startDate);
+
+            if(endDate.HasValue)
+                query = query.Where(c => c.EndDate == endDate);
+
+            return query
+                .Select(c => new ChoreDTO(c))
+                .ToList();
+        }
+
+        public IList<ChoreDTO> GetByUserId(int userId)
+        {
+            return _choreRepository
+                .GetByUserId(userId)
+                .Select(c => new ChoreDTO(c))
+                .ToList();
         }
 
         public ChoreDTO GetById(int id)
         {
-            throw new NotImplementedException();
+            Chore data = _choreRepository.GetById(id);
+
+            return new ChoreDTO(data);
         }
 
         public void Insert(ChoreDTO choreDTO)
         {
-            throw new NotImplementedException();
+            Chore data = new(choreDTO);
+
+            _choreRepository.Insert(data);
         }
 
         public void Update(ChoreDTO choreDTO, int id)
         {
-            throw new NotImplementedException();
+            Chore chore = _choreRepository.GetById(id);
+
+            chore.Name = choreDTO.Name;
+            chore.StartDate = choreDTO.StartDate;
+            chore.EndDate = choreDTO.EndDate;
+            chore.Status = choreDTO.Status;
+
+            _choreRepository.Update(chore);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            Chore data = _choreRepository.GetById(id);
+
+            _choreRepository.Delete(data);
         }
     }
 }
