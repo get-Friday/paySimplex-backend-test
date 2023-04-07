d58c3f475a547c54334009fe1ec505b98866bc16
get.Friday <gabriel.getdev@gmail.com>
2023-04-06 22:52:26 -0300

    Feat: interface and service for chore

---
+++ paySimplex.Domain/Interfaces/Services/IChoreService.cs
@@ -0,0 +1,13 @@
+﻿using paySimplex.Domain.DTOs;
+
+namespace paySimplex.Domain.Interfaces.Services
+{
+    public interface IChoreService
+    {
+        IList<ChoreDTO> GetAll();
+        ChoreDTO GetById(int id);
+        void Insert(ChoreDTO chore);
+        void Update(ChoreDTO chore, int id);
+        void Delete(int id);
+    }
+}
---
+++ paySimplex.Domain/Services/ChoreService.cs
@@ -0,0 +1,40 @@
+﻿using paySimplex.Domain.DTOs;
+using paySimplex.Domain.Interfaces.Repositories;
+using paySimplex.Domain.Interfaces.Services;
+
+namespace paySimplex.Domain.Services
+{
+    internal class ChoreService : IChoreService
+    {
+        private readonly IChoreRepository _choreRepository;
+        public ChoreService(IChoreRepository choreRepository)
+        {
+            _choreRepository = choreRepository;
+        }
+
+        public IList<ChoreDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChoreDTO GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(ChoreDTO choreDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ChoreDTO choreDTO, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
