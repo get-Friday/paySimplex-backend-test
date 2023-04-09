e805fa136f42d01a6105bb238ba54753a381159e
get.Friday <gabriel.getdev@gmail.com>
2023-04-09 14:23:09 -0300

    Feat: create userDTO

---
+++ paySimplex.Domain/DTOs/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using paySimplex.Domain.Models;
+
+namespace paySimplex.Domain.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public UserDTO()
+        {
+        }
+        public UserDTO(User user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            Email = user.Email;
+        }
+    }
+}
