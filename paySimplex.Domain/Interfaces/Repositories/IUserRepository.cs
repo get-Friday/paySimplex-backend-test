174ef5627a143e08d24dd01de8498a6c4f39937a
get.Friday <gabriel.getdev@gmail.com>
2023-04-09 14:22:55 -0300

    Feat: create user repository

---
+++ paySimplex.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using paySimplex.Domain.Models;
+
+namespace paySimplex.Domain.Interfaces.Repositories
+{
+    internal interface IUserRepository : IBaseRepository<User, int>
+    {
+    }
+}
---
+++ paySimplex.Infra/Data/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using paySimplex.Domain.Interfaces.Repositories;
+using paySimplex.Domain.Models;
+
+namespace paySimplex.Infra.Data.Repositories
+{
+    public class UserRepository : BaseRepository<User, int>, IUserRepository
+    {
+        public UserRepository(PaySimplexDbContext context) : base(context)
+        {
+        }
+    }
+}
